800afc5b88a4802f384d5ed593dcf1426005a695
d.kantorowicz@odo24.pl <kotorozec.code@gmail.com>
2024-06-12 15:58:37 +0200

    controls ux

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+// const buttonContainer = document.querySelector(".player-controls");
+
+// function handlechange(e) {
+//   const button = e.target.closest("button");
+//   if(!button) {
+//     return
+//   }
+//   const ids = button.getAttribute("id");
+
+//   if ((ids === "play")) {
+//     console.log("Hi");
+//   }
+// }
+
+// buttonContainer.addEventListener("click", (e) => {
+//   handlechange(e);
+// });
