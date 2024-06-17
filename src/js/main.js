753f0cd91d4de4f9f018b59d3ac8b5ee81af769b
d.kantorowicz@odo24.pl <kotorozec.code@gmail.com>
2024-06-17 13:58:53 +0200

    Render logic, next and prev play and stop

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,114 @@
-const buttonContainer = document.querySelector(".player-controls");
+import songs from "./song.js";
 
-function controlsMusic(e) {
-  const playButton = e.target.closest("button#play");
-  const playIcon = playButton.querySelector("#playIcon");
-  const stopIcon = playButton.querySelector("#stopIcon");
-  if (!playButton) {
-    return;
+const buttonContainer = document.querySelector(".player-controls");
+const audio = document.querySelector("audio"); 
+
+function handleSong(song) {
+const playButton = document.querySelector("button#play")
+  const playIcon = document.getElementById("playIcon");
+  const stopIcon = document.getElementById("stopIcon");
+
+  const nextButton = document.querySelector("button#next")
+  const prevtButton = document.querySelector("button#prev")
+
+  let currentIndex = 0; 
+  
+  const title = document.querySelector("#music-title");
+  const artist = document.querySelector("#music-author");
+  const img = document.querySelector("#cover-img");
+
+  function renderSong() {
+    img.src = `src/img/${song[currentIndex].name}.jpg`;
+    title.textContent = song[currentIndex].displayName;
+    artist.textContent = song[currentIndex].artist;
+    audio.src = `src/mp3/${song[currentIndex].name}.mp3`;
+    img.onload = function() {
+      img.setAttribute("height", img.naturalHeight);
+      img.setAttribute("width", img.naturalWidth);
+    };
+    
   }
 
-  if (playButton.dataset.musicState === "play") {
-    playIcon.classList.replace("shown", "hidden");
-    stopIcon.classList.replace("hidden", "shown");
-    playButton.dataset.musicState = "stop";
-  } else {
+  function stopMusicButton() {
+
     playIcon.classList.replace("hidden", "shown");
     stopIcon.classList.replace("shown", "hidden");
+  
     playButton.dataset.musicState = "play";
+    playButton.setAttribute("title", "Play song");
+  
+  }
+
+  function playMusicButton()  {
+    playIcon.classList.replace("shown", "hidden");
+    stopIcon.classList.replace("hidden", "shown");
+  
+    playButton.dataset.musicState = "stop";
+    playButton.setAttribute("title", "Pause");
+  }
+  
+  function playAudio() {
+    audio.play()
+  }
+
+  function stopAudio() {
+    audio.pause()
+  }
+
+  function nextSong() {
+    currentIndex = (currentIndex + 1) % song.length;
+    renderSong()
+  }
+
+  
+  function prevSong() {
+    currentIndex = (currentIndex - 1 + song.length) % song.length;
+    renderSong()
+  }
+
+  return {
+    renderSong,
+    nextSong,
+    prevSong,
+    playMusicButton,
+    stopMusicButton,
+    playAudio,
+    stopAudio,
+  };
+}
+
+const player = handleSong(songs);
+player.renderSong();
+
+
+function controlsMusic(e) {
+  const playButton = e.target.closest("button");
+
+  if (!playButton) return;
+
+  switch (playButton.dataset.musicState) {
+    case "play":
+      player.playMusicButton();
+      player.playAudio();
+      break;
+
+    case "stop":
+      player.stopMusicButton();
+      player.stopAudio();
+      break;
+
+    case "next":
+      player.stopMusicButton()
+      player.nextSong()
+      break;
+
+    case "prev":
+      player.stopMusicButton()
+      player.prevSong()
+      break;
+
+    default:
+      console.error("Something goes wrong")
   }
 }
 
---
+++ src/js/song.js
@@ -0,0 +1,8 @@
+const songs = [
+    { name: "jacinto-1", displayName:"Electric Chill Machine", artist:"Jacinto Design" },
+    { name: "jacinto-2", displayName:"Seven Nation Army (remix)", artist:"Jacinto Design" },
+    { name: "jacinto-3", displayName:"Goodnight Disco Queen", artist:"Jacinto Design" },
+    { name: "metric-1", displayName:"Front Row", artist:"Metric/Jacinto Design" },
+];
+
+export default songs;
